9dd28c0391bb5525bb58faffbe8c939ed96d74c8
Dazannah <beattius@gmail.com>
2023-08-02 22:12:08 +0200

    started registration form (testing some stlye)

--- app/components/Registration.jsx
+++ app/components/Registration.jsx
@@ -6,19 +6,47 @@ function Registration(props) {
     const response = await Axios.post("/")
   }
 
+  function handleFocus(e) {
+    const inputInFocus = document.getElementById(`${e.target.id}-label`)
+
+    inputInFocus.classList.add("register-label-end")
+  }
+
   return (
-    <form action="">
-      <input type="text" name="username" id="username" />
-      <input type="password" name="password" id="password" />
-      <input type="password" name="passwordRepeate" id="passwordRepeate" />
-      <input type="email" name="email" id="email" />
+    <div id="register-form-wrapper">
+      <form id="register-form">
+        <div className="relative">
+          <label id="register-username-label" className="register-label register-label-starter" htmlFor="register-username">
+            Username
+          </label>
+          <input id="register-username" className="register-input" type="text" name="username" onFocus={e => handleFocus(e)} />
+        </div>
+        <div className="relative">
+          <label id="register-password-label" htmlFor="register-password" className="register-label register-label-starter">
+            Password
+          </label>
+          <input className="register-input" type="password" name="password" id="register-password" onFocus={e => handleFocus(e)} />
+        </div>
+        <div className="relative">
+          <label id="register-passwordRepeate-label" htmlFor="register-passwordRepeate" className="register-label register-label-starter">
+            Repeate password
+          </label>
+          <input type="password" name="passwordRepeate" id="register-passwordRepeate" className="register-input" onFocus={e => handleFocus(e)} />
+        </div>
+        <div className="relative">
+          <label id="register-email-label" htmlFor="register-email" className="register-label register-label-starter">
+            E-mail
+          </label>
+          <input type="email" name="email" id="register-email" className="register-input" onFocus={e => handleFocus(e)} />
+        </div>
 
-      <input type="submit" value="Submit" onClick={e => handleSubmit(e)} />
+        <input type="submit" value="Submit" id="register-submit" onClick={e => handleSubmit(e)} />
 
-      <Link id="to-login" to="/">
-        To login
-      </Link>
-    </form>
+        <Link id="to-login" to="/">
+          To login
+        </Link>
+      </form>
+    </div>
   )
 }
 
