85843310f2aa13111d7dd05adf249d8b6e9d9940
Dazannah <beattius@gmail.com>
2023-08-05 12:16:36 +0200

    post base + base layout

--- app/components/postComponents/Post.jsx
+++ app/components/postComponents/Post.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
 
 function Post(props) {
-  return (
-    <div className="single-post">
-      Title: {props.post.title}
-      PostBody: {props.post.postBody}
-      Author: {props.post.author}
-      Created: {props.post.postCreateDate.toLocaleString()}
-    </div>
-  )
+  if (typeof props.post === "array") {
+    return <>multiple post</>
+  } else {
+    return (
+      <div id="single-post-wrapper">
+        <div className="post-wrapper">
+          <h1>{props.post.postTitle}</h1>
+          <span className="post-body">{props.post.postBody}</span>
+          <Link className="post-username" key={props.post.author} to={"/profile/" + props.post.author}>
+            {props.post.author}
+          </Link>
+          <span className="post-create-date">Posted: {props.post.postCreateDate}</span>
+
+          <button className="post-watch-comments">Show comments</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Post
