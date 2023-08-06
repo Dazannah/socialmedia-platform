7db1e359b5ae7e4776a849e353930e3ed8762ccb
Dazannah <beattius@gmail.com>
2023-08-06 18:54:34 +0200

    error helper function extended

--- app/components/helperFunctions/errorHandler.js
+++ app/components/helperFunctions/errorHandler.js
@@ -1,23 +1,45 @@
 function errorHandler(error) {
-  let type = ""
-  let value = ""
-
-  if (error.response.status === 409) type = "warning"
-  if (error.response.status === 404) type = "warning"
-
-  if (error.response.data) {
-    if (Array.isArray(error.response.data)) {
-      value = error.response.data
-    } else {
-      value = [error.response.data]
-    }
-  }
+  let type = getType(error)
+  let value = getValue(error)
 
   if (value != "" && type != "") {
     return { type, value }
   } else {
     return { type: "", value: "" }
   }
+}
+
+function getType(error) {
+  let type = ""
+
+  if (error.response) {
+    if (error.response.status === 409) type = "warning"
+    if (error.response.status === 404) type = "warning"
+  } else {
+    type = "warning"
+  }
+
+  return type
+}
+
+function getValue(error) {
+  let value = ""
+
+  if (error.response) {
+    if (error.response.data) {
+      if (Array.isArray(error.response.data)) {
+        value = error.response.data
+      } else {
+        value = [error.response.data]
+      }
+    } else {
+      value = error
+    }
+  } else {
+    value = error
+  }
+
+  return value
 }
 
 export default errorHandler
