0a4b9873a006272405697f0d394f2d70c5affc6f
Dazannah <beattius@gmail.com>
2023-08-11 10:11:26 +0200

    added follow button to profile

--- app/components/profile/ProfileData.jsx
+++ app/components/profile/ProfileData.jsx
@@ -1,8 +1,37 @@
-import React, { useEffect } from "react"
+import React, { useContext, useEffect } from "react"
+
+import Axios from "axios"
+
+import StateContext from "../../StateContext.jsx"
 
 function ProfileData(props) {
-  console.log(props.user)
-  return <div id="profile-data-wrapper">{props.user.username}</div>
+  const appState = useContext(StateContext)
+
+  async function startFollow() {
+    try {
+      await Axios.post(
+        `/start-follow/${props.user.username}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${appState.token}`
+          }
+        }
+      )
+    } catch (err) {}
+  }
+  return (
+    <div id="profile-data-wrapper">
+      <span className="profile-username">{props.user.username}</span>
+      <button
+        onClick={() => {
+          startFollow()
+        }}
+      >
+        Follow
+      </button>
+    </div>
+  )
 }
 
 export default ProfileData
