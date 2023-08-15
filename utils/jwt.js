3bc99324f52da2f0a61284ed71e29d1993614431
Dazannah <beattius@gmail.com>
2023-08-15 16:47:39 +0200

    jwt expiration typo corrected

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken")
 function generateJwt(data) {
   return jwt.sign(
     {
-      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 60, //1 day
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, //1 day
       data: {
         username: data.username,
         userId: data.userId
